a6d99ba48ca8a41430120b67f20d92db2b7dfc6e
kuritaeiji <eiji199825@yahoo.co.jp>
2022-02-18 12:02:38 +0900

    修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(ENV['FRONT_ORIGIN'])
+    origins(ENV['FRONT_ORIGIN'] || 'pfc-calculator-front.herokuapp.com')
 
     resource '*',
       headers: :any,
