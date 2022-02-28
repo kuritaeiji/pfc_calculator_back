a39e5d2e6c0aee8ac2a4257cc5dedddda7f1f2d2
kuritaeiji <eiji199825@yahoo.co.jp>
2022-02-28 17:20:14 +0900

    dayファクトリーの修正

--- spec/factories/days.rb
+++ spec/factories/days.rb
@@ -1,9 +1,6 @@
 FactoryBot.define do
   factory :day do
-    sequence(:date) do |n|
-      "2022-01-0#{n}" if n < 10
-      "2022-01-#{n}"
-    end
+    sequence(:date) { |n| Date.today - n.day }
     user
   end
 end
