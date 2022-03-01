4209430a8fd96b21bc0ab7fa74a7f582d341abb7
kuritaeiji <eiji199825@yahoo.co.jp>
2022-03-01 18:15:52 +0900

    dayモデルにcaloryやpfcメソッドを追加

--- app/models/day.rb
+++ app/models/day.rb
@@ -5,4 +5,14 @@ class Day < ApplicationRecord
   has_many(:dishes, dependent: :destroy)
 
   validates(:date, presence: true, uniqueness: { scope: :user })
+
+  class << self
+    def define_attrs(*attr_names)
+      attr_names.each do |attr_name|
+        define_method(attr_name) { ate_foods.eager_load(:food).to_a.sum(&attr_name) + dishes.sum(attr_name) }
+      end
+    end
+  end
+
+  define_attrs(:calory, :protein, :fat, :carbonhydrate)
 end
