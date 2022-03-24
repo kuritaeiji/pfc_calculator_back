fbd79f5636b25c406f033a20c3a026449e07b926
kuritaeiji <eiji199825@yahoo.co.jp>
2022-03-24 20:48:48 +0900

    修正

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# ActionMailer::Base.smtp_settings = {
-#   user_name:             'apikey',
-#   password: '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',
-#   domain: 'outlook.com',
-#   address: 'smtp.sendgrid.net',
-#   port: 587,
-#   authentication: :plain,
-#   enable_starttls_auto: true
-# }
